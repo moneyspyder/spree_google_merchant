3b1b1ab469eb2e196ecd43d7d96e2d3f395121fa
Sam <samtreweek@gmail.com>
2012-05-28 11:54:25 +0100

    some changes

--- app/controllers/spree/products_decorator.rb
+++ app/controllers/spree/products_decorator.rb
@@ -1,5 +1,10 @@
 Spree::ProductsController.class_eval do
   def google_merchant
-    @products = Product.active
+    @products = Spree::Product.active
+
+    respond_to do |format|
+      format.rss { render :layout => false }
+    end
+
   end
 end
