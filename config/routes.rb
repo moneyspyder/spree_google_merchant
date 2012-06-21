a798822135f6ba8916a5d1c9fb6acca60e3e6d25
Alberto Vena <kennyadsl@gmail.com>
2012-06-21 14:12:41 +0300

    fixes routes to get rss feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Spree::Core::Engine.routes.append do
-  resources :products do 
-      get :google_merchant
-  end
+  match "google_merchant", :to => 'products#google_merchant'
 
   namespace :admin do
     resource :google_merchants
