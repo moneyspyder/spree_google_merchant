a89725ffd658ade4f6f37d8158de533a9f684983
John Hawthorn <john@freerunningtechnologies.com>
2013-08-20 15:06:25 -0700

    Move merchant feed out of products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Spree::Core::Engine.routes.append do
-  match "google_merchant", :to => 'products#google_merchant'
+Spree::Core::Engine.routes.draw do
+  get "google_merchant" => 'google_merchant#index', :constraints => {:format => :rss}
 
   namespace :admin do
     resource :google_merchants
